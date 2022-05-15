71024f62e8c0c1dda3292c4e74df2a25083bafd1
hyebinyu <hyebinyu1110@gmail.com>
2022-05-16 00:36:50 +0900

    #14.2 Building the Navigation (05:36)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, {Supspense, lazy} from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from "./components/Home"
+import About from "./routes/About";
+import Navigation from "./routes/Navigation";
 
-function App(){
-  return <span>React JS is the best!</span>
+import "./App.css";
+
+class App extends React.Component{
+ state={
+   pathId: ""
+
+  }
+  render(){
+    
+  return (
+  <div>
+  <Router >
+    <Navigation />
+    <Switch>
+      <Route exact={true} path="/about" component={About} />
+      <Route exact={true} path="/" component={Home} />
+    </Switch>
+  </Router>
+  </div>
+  )
 }
+}
+
 export default App;
